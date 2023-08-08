43f2431fa447d9ac43dfd80344eb6bd4351089b3
Maksim Zinal <maxzinal@yandex.ru>
2023-08-08 11:09:38 +0300

    config backup on update

--- run-update-config.sh
+++ run-update-config.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 
-ansible-playbook -b -i hosts -f 20 update-config.yaml
+BACKUP_LABEL=`date '+%Y-%m-%d_%H-%M-%S'`
+ansible-playbook -b -i hosts -f 20 update-config.yaml --extra-vars "ydb_config_backup=${BACKUP_LABEL}"
