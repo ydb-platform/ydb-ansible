69548c02cca3cffcd2d3feabedf1ab31ed15d7d2
Eugene Arbatsky <elabpro@yandex-team.ru>
2024-10-22 17:15:16 +0800

    KIKIMR-22100 execute command in ydbd

---
+++ plugins/modules/ydbd_cmd.py
@@ -0,0 +1,32 @@
+import os
+
+
+from ansible.module_utils.basic import AnsibleModule
+from ansible_collections.ydb_platform.ydb.plugins.module_utils import drive, cli
+
+
+def main():
+    argument_spec = dict(
+        cmd=dict(type='str', default='')
+    )
+    cli.YDBD.add_arguments(argument_spec)
+    module = AnsibleModule(argument_spec=argument_spec, supports_check_mode=False)
+    result = {'changed': False}
+    try:
+        
+        ydbd_cli = cli.YDBD.from_module(module)
+        cmd = module.params.get('cmd')
+
+        rc,stdout,_ = ydbd_cli(cmd.split(" "))
+        result['stdout'] = stdout
+        result['changed'] = True
+
+        module.exit_json(**result)
+
+    except Exception as e:
+        result['msg'] = f'unexpected exception: {e}'
+        module.fail_json(**result)
+
+
+if __name__ == '__main__':
+    main()
