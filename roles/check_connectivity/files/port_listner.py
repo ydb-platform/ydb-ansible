452561366b81708e0972bb6ee10ee48f7fbbef60
Eugene Arbatsky <elabpro@yandex-team.ru>
2024-11-22 16:45:22 +0800

    dstool install from binary

--- roles/check_connectivity/files/port_listner.py
+++ roles/check_connectivity/files/port_listner.py
@@ -1,10 +1,15 @@
+# Simple script for checking connectivity
+# It's starting TCP server to listen several ports
+# Usage:
+#   python3 port_listner.py <port1> [..<portN>]
+#
 import socket
 import threading
 import sys
 
 def listen_port(port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('0.0.0.0', port))
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)  
+    sock.bind(('::', port)) 
     sock.listen(5)  
 
     print(f"Listen port {port}...")
