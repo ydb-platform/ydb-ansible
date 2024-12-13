108ce628dd78b8b52f69b64e462baa07276d7d9c
Maksim Zinal <zinal@ydb.tech>
2024-12-13 22:05:21 +0300

    db creation via command args

--- run-create-database.sh
+++ run-create-database.sh
@@ -1,4 +1,16 @@
 #! /bin/sh
 
 . ./run-active-options.sh
-${RUN_ANSIBLE} create-database.yaml
+
+set +e
+set +u
+
+if [ -z "$1" ]; then
+  ${RUN_ANSIBLE} create-database.yaml
+elif [ -z "$2" ]; then
+  ${RUN_ANSIBLE} create-database.yaml --extra-vars "ydb_dbname=$1"
+elif [ -z "$3" ]; then
+  ${RUN_ANSIBLE} create-database.yaml --extra-vars "ydb_dbname=$1 ydb_default_groups=$2"
+else
+  ${RUN_ANSIBLE} create-database.yaml --extra-vars "ydb_dbname=$1 ydb_default_groups=$2 ydb_pool_kind=$3"
+fi
