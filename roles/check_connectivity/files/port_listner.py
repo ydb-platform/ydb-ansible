156087e0ea65046cf2176638507c555c4444eb62
Eugene Arbatsky <elabpro@yandex-team.ru>
2025-01-24 23:15:24 +0800

    Fix for python3.7

--- roles/check_connectivity/files/port_listner.py
+++ roles/check_connectivity/files/port_listner.py
@@ -7,19 +7,40 @@ import socket
 import threading
 import sys
 
-def listen_port(port):
-    addr = ("", port) 
-    if socket.has_dualstack_ipv6():
-        sock = socket.create_server(addr, family=socket.AF_INET6, dualstack_ipv6=True)
-    else:
-        sock = socket.create_server(addr)  
+def listen_port4(port):
+    try:
+        # Set up IPv4 address
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(("0.0.0.0", port))
+        s.listen(5)
 
-    print(f"Listen port {port}...")
+        print(f"Listen port IPv4 {port}...")
 
-    while True:
-        conn, addr = sock.accept()
-        with conn:
-            print(f"New connection from {addr}")
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print(f"New connection from {addr}")
+            conn.close()
+    except:
+        print(f"No IPv4")
+
+def listen_port6(port):
+    try:
+        # Set up IPv6 address
+        s = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        s.bind(("::", port))
+        s.listen(5)
+
+        print(f"Listen port IPv6 {port}...")
+
+        while True:
+            conn, addr = s.accept()
+            with conn:
+                print(f"New connection from {addr}")
+            conn.close()
+    except:
+        print(f"No IPv6")
+
 
 def main():
     if len(sys.argv) < 2:
@@ -31,7 +52,10 @@ def main():
     threads = []
 
     for port in ports:
-        thread = threading.Thread(target=listen_port, args=(port,))
+        thread = threading.Thread(target=listen_port4, args=(port,))
+        threads.append(thread)
+        thread.start()
+        thread = threading.Thread(target=listen_port6, args=(port,))
         threads.append(thread)
         thread.start()
 
