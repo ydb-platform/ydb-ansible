a8146666b461e7c00dd2c32aedd6827d02f01ef0
Eugene Arbatsky <elabpro@yandex-team.ru>
2025-06-05 18:17:59 +0800

    refactoring

--- README.md
+++ README.md
@@ -555,10 +555,10 @@ all:
    A2: Add `ydb_storage_update_config: True` in inventory
 
 4) Q: Restart takes a lot of time, no output, no activities. What to do?
-   A: You can switch on logging in this case - add --extra-vars "ydbops_log=True" to your command and logs will be recorded into `/tmp/ydbops.log`
+   A: You can switch on logging in this case - add --extra-vars "ydbops_log=/tmp/ydbops.log" to your command and logs will be recorded into `/tmp/ydbops.log`
    A: Also you can change availability mode from `weak` to `force`. Be aware - this option could lead to cluster unavailability for some time.
 
    ```bash
-   ansible-playbook ydb_platform.ydb.restart --extra-vars "ydbops_log=True"
+   ansible-playbook ydb_platform.ydb.restart --extra-vars "ydbops_log=/tmp/ydbops.log"
    ansible-playbook ydb_platform.ydb.restart --extra-vars "availability_mode=force"
    ```
--- plugins/module_utils/cli.py
+++ plugins/module_utils/cli.py
@@ -150,12 +150,11 @@ class YdbOps(CLI):
         token=dict(type='str', default=None, no_log=True),
         token_file=dict(type='str', default=None),
         hosts=dict(type='str',default=None),
-        log=dict(type='bool',default=False),
-        logfile=dict(type='str',default=None),
+        log=dict(type='str',default=False),
     )
 
     def __init__(self, module, ydbops_bin, ydbops_endpoint, ydbops_systemd_unit=None,
-                 ca_file=None, ssh_args=None, availability_mode=None, token=None, token_file=None, hosts=None, log=False, logfile=None):
+                 ca_file=None, ssh_args=None, availability_mode=None, token=None, token_file=None, hosts=None, log=None):
         self.module = module
 
         self.common_options = [ydbops_bin, 'restart', '--storage']
@@ -177,6 +176,6 @@ class YdbOps(CLI):
             self.common_environ['YDB_TOKEN'] = token
         elif token_file is not None:
             self.common_options.extend(['--token-file', token_file])
-        if log and logfile is not None:
-            self.cmd_format = "{cmd} >> " + shlex.quote(logfile)
+        if log is not None and log != "":
+            self.cmd_format = "{cmd} >> " + shlex.quote(log)
             self.use_unsafe_shell = True
